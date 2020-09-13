b037f241981e631b13cf083c93a44d3f6c6200c2
npuZhangYuFei <156125813@qq.com>
2020-09-13 10:28:29 +0800

    init project

---
+++ src/store/looklook/learn/hashmap/test.java
@@ -0,0 +1,23 @@
+package store.looklook.learn.hashmap;
+
+import java.util.HashMap;
+
+/**
+ * @author ：zyf
+ * @date ：Created in 2020/9/13
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+
+public class test {
+
+    public static void main(String[] args) {
+        HashMap<String, Double> hashMap = new HashMap<>();
+        System.out.println(7);
+
+        hashMap.put("a", 1.0);
+        hashMap.put("a", 2.1);
+
+    }
+}
